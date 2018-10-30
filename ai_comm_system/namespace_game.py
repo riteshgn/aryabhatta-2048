44e8180b67a1b910d9f40d9bf59e62bef604f3bd
Ritesh Nair <riteshgn@gmail.com>
2018-10-30 11:26:37 -0400

    - Game namespace will emit message to only the client which requested the next move - some code refactoring to variable naming conventions and print statements

--- ai_comm_system/namespace_game.py
+++ ai_comm_system/namespace_game.py
@@ -6,10 +6,11 @@ class GameNamespace(socketio.AsyncNamespace):
 
     def __init__(self, namespace):
         super(GameNamespace, self).__init__(namespace)
+        self._namespace = namespace
         self._game_messages = GameMessages()
 
     def on_connect(self, sid, environ):
-        print("client connected to game namespace ", sid)
+        print('client connected to game namespace ', sid)
 
     def on_disconnect(self, sid):
         print('client disconnected from game namespace ', sid)
@@ -18,4 +19,10 @@ class GameNamespace(socketio.AsyncNamespace):
         result = self._game_messages.handle(data)
 
         if result.get('respond', False):
-            await self.emit('message', result.get('payload', None), namespace='/game')
+            # when room=sid, message will be emitted back to only that client
+            await self.emit(
+                'message',
+                result.get('payload', None),
+                namespace=self._namespace,
+                room=sid
+            )
